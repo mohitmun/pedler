820f634beb6b9f6343b03fa605f1b55d82649630
Mohit M <mohitcrox@gmail.com>
2017-06-17 00:00:40 +0530

    bot configured

---
+++ config/initializers/assets.rb
@@ -0,0 +1,23 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+unless Rails.env.production?
+  bot_files = Dir[Rails.root.join('app', 'bot', '**', '*.rb')]
+  bot_reloader = ActiveSupport::FileUpdateChecker.new(bot_files) do
+    bot_files.each{ |file| require_dependency file }
+  end
+
+  # ActiveSupport::Reloader.to_prepare do
+  #   bot_reloader.execute_if_updated
+  # end
+
+  bot_files.each { |file| require_dependency file }
+end
