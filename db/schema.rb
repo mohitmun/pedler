83dda4660dd24afa197c17db2ea7e4817b581d5d
Mohit M <mohitcrox@gmail.com>
2017-06-18 17:58:50 +0530

    misc

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,20 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617061922) do
+ActiveRecord::Schema.define(version: 20170618073139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.text     "first_name"
-    t.text     "last_name"
-    t.json     "json_store"
-    t.integer  "fb_id"
-    t.text     "contact"
-    t.text     "role"
+  create_table "groceries", force: :cascade do |t|
+    t.text "name"
+    t.integer "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_grocery_mappings", force: :cascade do |t|
+    t.integer "grocery_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.text "first_name"
+    t.text "last_name"
+    t.json "json_store"
+    t.text "contact"
+    t.text "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "fb_id"
   end
 
 end
---
+++ test/models/user_grocery_mapping_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserGroceryMappingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
